9c0840bdaafc6bc9100627945b6a3ad7b9accb45
Amy Hunter <amyrhunter@gmail.com>
2016-02-22 16:08:47 -0800

    Twitter login is working. Rspec tests are passing. 100% code coverage.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,6 @@ class User < ActiveRecord::Base
     if !user.nil?
       # Spy found, continue on with your life
       return user
-    elsif auth_hash["provider"] == "developer"
-      # Create a new spy
-      user            = User.new
-      user.uid        = auth_hash["uid"]
-      user.provider   = auth_hash["provider"]
-      user.username   = auth_hash["info"]["username"]
-      user.image_url = auth_hash["info"]["image"]
-      if !user.image_url.present?
-        user.image_url = "blank.png"
-      end
-      if user.save
-        return user
-      else
-        return nil
-      end
     elsif auth_hash["provider"] == "twitter"
       user           = User.new
       user.uid        = auth_hash["uid"]
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,40 +2,6 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "GET #create" do
-    context "when using developer authentication" do
-      context "is successful" do
-        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:developer] }
-
-        it "redirects to the home page" do
-          get :create, provider: :developer
-          expect(response).to redirect_to root_path
-        end
-
-        it "goes to login page" do
-          get :new, provider: :developer
-          expect(response).to redirect_to "http://test.host/auth/"
-        end
-
-        it "creates a user" do
-          expect { get :create, provider: :developer }.to change(User, :count).by(1)
-        end
-
-        it "doesn't create a new user if user already exists" do
-          User.create(uid: "123545", provider: "developer", username: "King Dedede")
-          expect { get :create, provider: :developer }.to change(User, :count).by(0)
-        end
-
-        it "assigns the session[:user_id]" do
-          get :create, provider: :developer
-          expect(session[:user_id]).to eq assigns(:user).id
-        end
-
-        it "logs out" do
-          delete :destroy
-          expect(session[:user_id]).to eq nil
-        end
-      end
-    end
       context "when using twitter authentication" do
         context "is successful" do
           before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter] }
