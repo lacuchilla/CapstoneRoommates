fd6cc40996d572484482bea5de231e151690b8f6
Amy Hunter <amyrhunter@gmail.com>
2016-02-25 18:01:59 -0800

    Add user id to shares model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226013751) do
+ActiveRecord::Schema.define(version: 20160226014326) do
 
   create_table "bills", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160226013751) do
     t.boolean  "paid"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
