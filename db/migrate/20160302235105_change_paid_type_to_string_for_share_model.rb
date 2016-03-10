ee060cd4bb4b0a26dd52cc4ce44f7c97d13a5cb6
Amy Hunter <amyrhunter@gmail.com>
2016-03-09 16:41:15 -0800

    deleted migrations that changed boolean to string and back to boolean. Kept original status of paid share as boolean

--- db/migrate/20160302235105_change_paid_type_to_string_for_share_model.rb
+++
@@ -1,5 +0,0 @@
-class ChangePaidTypeToStringForShareModel < ActiveRecord::Migration
-  def change
-    change_column :shares, :paid, :string
-  end
-end
--- db/migrate/20160303222123_change_paid_to_boolean_for_share.rb
+++
@@ -1,5 +0,0 @@
-class ChangePaidToBooleanForShare < ActiveRecord::Migration
-  def change
-    change_column :shares, :paid, :boolean
-  end
-end
