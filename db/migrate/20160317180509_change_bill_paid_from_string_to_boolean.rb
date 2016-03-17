49bd07bf82489498a0495e14915f09492a3fd167
Amy Hunter <amyrhunter@gmail.com>
2016-03-17 11:35:42 -0700

    Change wording in migration to boolean from string for bill. This will not work locally. Must switch this line out for the ommented out line if you want to run locally.

--- db/migrate/20160317180509_change_bill_paid_from_string_to_boolean.rb
+++ db/migrate/20160317180509_change_bill_paid_from_string_to_boolean.rb
@@ -1,6 +1,6 @@
 class ChangeBillPaidFromStringToBoolean < ActiveRecord::Migration
   def change
-    # change_column :bills, :paid, 'boolean USING CAST(paid AS boolean)'
-    change_column :bills, :paid, :boolean
+    change_column :bills, :paid, 'boolean USING CAST(paid AS boolean)'
+    # change_column :bills, :paid, :boolean
   end
 end
